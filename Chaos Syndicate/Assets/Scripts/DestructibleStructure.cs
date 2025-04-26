f6704a7cc56adccf80bf057640c3e9e41ae7ca63
Serban Orza <serban.orza@gmail.com>
2025-04-26 13:43:16 +0200

    added something to avoid infinite loop on objects

--- Chaos Syndicate/Assets/Scripts/DestructibleStructure.cs
+++ Chaos Syndicate/Assets/Scripts/DestructibleStructure.cs
@@ -2,22 +2,24 @@ using UnityEngine;
 
 public class DestructibleStructure : MonoBehaviour,Destructible
 {
+    private bool destroyed=false;
     public GameObject parts;
     public DestructibleStructure[] destroyNext;
     void Start()
     {
+        destroyed=false;
         parts.SetActive(false);
     }
     public void impact()
     {
+        if(destroyed)
+            return;
+        destroyed=true;
         //destroy
         parts.SetActive(true);
         //destroy other parts
         foreach(DestructibleStructure d in destroyNext)
             d.impact();
         Destroy(gameObject);
-    }
-    private void OnCollisionEnter(Collision other)
-    {
     }
 }
